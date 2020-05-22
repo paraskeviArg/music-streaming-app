b7ed308033d29aae69d3414c6d82ff8fd59511c2
paraskeviArg <paraskevi.arg98@gmail.com>
2020-05-22 17:55:49 +0300

    dw

--- app/src/main/java/com/example/projectb/SongList.java
+++ app/src/main/java/com/example/projectb/SongList.java
@@ -24,14 +24,18 @@ public class SongList extends ArrayAdapter<Song> {
     public View getView(int position, View view, ViewGroup parent) {
         LayoutInflater inflater = context.getLayoutInflater();
         View rowView = inflater.inflate(R.layout.song_row, null, true);
-        TextView txtTitle = rowView.findViewById(R.id.txt);
-        TextView txtTitle1 = rowView.findViewById(R.id.txt2);
+        TextView txtTrack = rowView.findViewById(R.id.trackName);
+        TextView textArtist = rowView.findViewById(R.id.artistName);
+        TextView textAlbum = rowView.findViewById(R.id.albumName);
         System.out.println("trackname " + array[position].getTrackName());
-        txtTitle.setText(array[position].getTrackName());
+        txtTrack.setText(array[position].getTrackName());
         System.out.println("artistname " + array[position].getArtistName());
-        txtTitle1.setText(array[position].getArtistName());
-        txtTitle.setTextSize(20);
-        txtTitle1.setTextSize(20);
+        textArtist.setText("Artist: " + array[position].getArtistName());
+        System.out.println("textAlbum " + array[position].getAlbumInfo());
+        textAlbum.setText("Album: " + array[position].getAlbumInfo());
+        txtTrack.setTextSize(20);
+        textArtist.setTextSize(15);
+        textAlbum.setTextSize(15);
         return rowView;
     }
 }
